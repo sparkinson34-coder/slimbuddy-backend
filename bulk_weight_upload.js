edca5ca499755a561980fb28bd204ad253dd4c0e
Sharon Parkinson <sparkinson34@gmail.com>
2025-08-18 08:09:23 +0100

    Updated bul_upload.js routes

--- bulk_weight_upload.js
+++ bulk_weight_upload.js
@@ -8,49 +8,49 @@ const JWT_TOKEN = process.env.JWT_TOKEN;
 // ✅ Input weight data here
 // ✅ Field Names - Date, Weight (in stones & pounds)
 const weightEntries = [
-{ date: "04-09-2024", weight: "17 st 4.5 lbs" notes: "1/2 Stone Award" }
-  { date: "11-09-2024", weight: "17 st 0 lbs" notes: " " }
-  { date: "18-09-2024", weight: "16 st 11.5 lbs" notes: "1 Stone Award" }
-  { date: "25-09-2024", weight: "16 st 7.5 lbs" notes: " " }
-  { date: "02-10-2024", weight: "16 st 6 lbs" notes: " " }
-  { date: "18-10-2024", weight: "16 st 1.5 lbs" notes: "1 1/2 Stone Award" }
-  { date: "24-10-2024", weight: "16 st 0 lbs" notes: " " }
-  { date: "30-10-2024", weight: "15 st 12 lbs" notes: "2 Stone Award" }
-  { date: "06-11-2024", weight: "15 st 11.5 lbs" notes: " " }
-  { date: "22-11-2024", weight: "15 st 5 lbs" notes: "2 1/2 Stone Award" }
-  { date: "27-11-2024", weight: "15 st 5 lbs" notes: " " }
-  { date: "04-12-2024", weight: "15 st 3 lbs" notes: " " }
-  { date: "13-12-2024", weight: "15 st 2 lbs" notes: " " }
-  { date: "18-12-2024", weight: "15 st 2 lbs" notes: " " }
-  { date: "24-12-2024", weight: "14 st 13 lbs" notes: " " }
-  { date: "31-12-2024", weight: "14 st 13 lbs" notes: " " }
-  { date: "07-01-2025", weight: "14 st 11 lbs" notes: "3 Stone Award" }
-  { date: "14-01-2025", weight: "14 st 10.5 lbs" notes: " " }
-  { date: "21-01-2025", weight: "14 st 10 lbs" notes: " " }
-  { date: "28-01-2025", weight: "14 st 8.5 lbs" notes: " " }
-  { date: "04-02-2025", weight: "14 st 7.5 lbs" notes: " " }
-  { date: "14-02-2025", weight: "14 st 6 lbs" notes: " " }
-  { date: "19-02-2025", weight: "14 st 6.5 lbs" notes: " " }
-  { date: "25-02-2025", weight: "14 st 3.5 lbs" notes: "3 1/2 Stone Award" }
-  { date: "05-03-2025", weight: "14 st 3.5 lbs" notes: " " }
-  { date: "12-03-2025", weight: "14 st 4 lbs" notes: " " }
-  { date: "21-03-2025", weight: "14 st 1 lbs" notes: " " }
-  { date: "09-04-2025", weight: "13 st 13 lbs" notes: " " }
-  { date: "18-04-2025", weight: "13 st 12 lbs" notes: "4 Stone Award" }
-  { date: "25-04-2025", weight: "13 st 12 lbs" notes: " " }
-  { date: "02-05-2025", weight: "13 st 11.5 lbs" notes: " " }
-  { date: "09-05-2025", weight: "13 st 11.5 lbs" notes: " " }
-  { date: "16-05-2025", weight: "13 st 10 lbs" notes: " " }
-  { date: "23-05-2025", weight: "13 st 9.5 lbs" notes: " " }
-  { date: "30-05-2025", weight: "13 st 9 lbs" notes: " " }
-  { date: "07-06-2025", weight: "13 st 8 lbs" notes: " " }
-  { date: "14-06-2025", weight: "13 st 9.5 lbs" notes: " " }
-  { date: "21-06-2025", weight: "13 st 5.5 lbs" notes: "4 1/2 Stone Award" }
-  { date: "28-06-2025", weight: "13 st 4.5 lbs" notes: " " }
-  { date: "05-07-2025", weight: "13 st 2.5 lbs" notes: " " }
-  { date: "12-07-2025", weight: "13 st 1 lbs" notes: " " }
-  { date: "19-07-2025", weight: "13 st 0 lbs" notes: " " }
-  { date: "26-07-2025", weight: "12 st 11.5 lbs" notes: "4 1/2 Stone Award" }
+{ date: "04-09-2024", weight: "17 st 4.5 lbs", notes: "1/2 Stone Award" },
+  { date: "11-09-2024", weight: "17 st 0 lbs", notes: " " },
+  { date: "18-09-2024", weight: "16 st 11.5 lbs", notes: "1 Stone Award" },
+  { date: "25-09-2024", weight: "16 st 7.5 lbs", notes: " " },
+  { date: "02-10-2024", weight: "16 st 6 lbs", notes: " " },
+  { date: "18-10-2024", weight: "16 st 1.5 lbs", notes: "1 1/2 Stone Award" },
+  { date: "24-10-2024", weight: "16 st 0 lbs", notes: " " },
+  { date: "30-10-2024", weight: "15 st 12 lbs", notes: "2 Stone Award" },
+  { date: "06-11-2024", weight: "15 st 11.5 lbs", notes: " " },
+  { date: "22-11-2024", weight: "15 st 5 lbs", notes: "2 1/2 Stone Award" },
+  { date: "27-11-2024", weight: "15 st 5 lbs", notes: " " },
+  { date: "04-12-2024", weight: "15 st 3 lbs", notes: " " },
+  { date: "13-12-2024", weight: "15 st 2 lbs", notes: " " },
+  { date: "18-12-2024", weight: "15 st 2 lbs", notes: " " },
+  { date: "24-12-2024", weight: "14 st 13 lbs", notes: " " },
+  { date: "31-12-2024", weight: "14 st 13 lbs", notes: " " },
+  { date: "07-01-2025", weight: "14 st 11 lbs", notes: "3 Stone Award" },
+  { date: "14-01-2025", weight: "14 st 10.5 lbs", notes: " " },
+  { date: "21-01-2025", weight: "14 st 10 lbs", notes: " " },
+  { date: "28-01-2025", weight: "14 st 8.5 lbs", notes: " " },
+  { date: "04-02-2025", weight: "14 st 7.5 lbs", notes: " " },
+  { date: "14-02-2025", weight: "14 st 6 lbs", notes: " " },
+  { date: "19-02-2025", weight: "14 st 6.5 lbs", notes: " " },
+  { date: "25-02-2025", weight: "14 st 3.5 lbs", notes: "3 1/2 Stone Award" },
+  { date: "05-03-2025", weight: "14 st 3.5 lbs", notes: " " },
+  { date: "12-03-2025", weight: "14 st 4 lbs", notes: " " },
+  { date: "21-03-2025", weight: "14 st 1 lbs", notes: " " },
+  { date: "09-04-2025", weight: "13 st 13 lbs", notes: " " },
+  { date: "18-04-2025", weight: "13 st 12 lbs", notes: "4 Stone Award" },
+  { date: "25-04-2025", weight: "13 st 12 lbs", notes: " " },
+  { date: "02-05-2025", weight: "13 st 11.5 lbs", notes: " " },
+  { date: "09-05-2025", weight: "13 st 11.5 lbs", notes: " " },
+  { date: "16-05-2025", weight: "13 st 10 lbs", notes: " " },
+  { date: "23-05-2025", weight: "13 st 9.5 lbs", notes: " " },
+  { date: "30-05-2025", weight: "13 st 9 lbs", notes: " " },
+  { date: "07-06-2025", weight: "13 st 8 lbs", notes: " " },
+  { date: "14-06-2025", weight: "13 st 9.5 lbs", notes: " " },
+  { date: "21-06-2025", weight: "13 st 5.5 lbs", notes: "4 1/2 Stone Award" },
+  { date: "28-06-2025", weight: "13 st 4.5 lbs", notes: " " },
+  { date: "05-07-2025", weight: "13 st 2.5 lbs", notes: " " },
+  { date: "12-07-2025", weight: "13 st 1 lbs", notes: " " },
+  { date: "19-07-2025", weight: "13 st 0 lbs", notes: " " },
+  { date: "26-07-2025", weight: "12 st 11.5 lbs", notes: "4 1/2 Stone Award" }
 ];
 
 function convertDate(dateStr) {
